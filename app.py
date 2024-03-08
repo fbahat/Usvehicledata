381d764bc71add64da4fe5ee5309a3d088fb72ff
Furkan <45549538+fbahat@users.noreply.github.com>
2024-03-08 17:47:19 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import streamlit as st
+import altair as at
+import plotly.express as py
+import numpy as np
+
+
+vehicles = pd.read_csv("C:/Users/Furkan/Usvehicledata/vehicles_us.csv")
+
+print(vehicles.head(10))
+
+vehicles.isna()
+vehicles.isna().sum()
+vehicles.duplicated().sum() 
+
+vehicles['fuel'].unique()
+
+# Histogram of price
+price_histogram = py.histogram(vehicles, x='price', nbins=20, title='Price Distribution')
+st.plotly_chart(price_histogram)
+
+# Histogram of odometer
+odometer_histogram = py.histogram(vehicles, x='odometer', nbins=20, title='Odometer Distribution')
+st.plotly_chart(odometer_histogram)
+
+# Scatterplot of price vs odometer
+price_vs_odometer_scatter = py.scatter(vehicles, x='price', y='odometer', title='Price vs Odometer')
+st.plotly_chart(price_vs_odometer_scatter)
+
+# Scatterplot of price vs model_year
+price_vs_model_year_scatter = py.scatter(vehicles, x='price', y='model_year', title='Price vs Model Year')
+st.plotly_chart(price_vs_model_year_scatter)
